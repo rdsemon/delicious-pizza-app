72cbd6a059e27000a6b6dcf9466e2a47ee03452f
rdsemon <codewithemon@gmail.com>
2025-06-19 21:09:56 +0600

    fixed the resetPassword link path

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -15,7 +15,7 @@ const sendMail = async function (userMail, restToken, expireDate) {
       from: 'Delicious pizza <deliciouspizza@gmail.com>',
       to: `${userMail}`,
       subject: 'Reset your password',
-      text: `click the below link to reset your password. it will exprie in 10 munites \n http:127.0.0.1/api/v1/pizza/resetpassword/${restToken}. \n It will expire in ${expireDate}`,
+      text: `click the below link to reset your password. it will exprie in 10 munites \n http://127.0.0.1:3000/api/v1/user/resetpassword/${restToken}. \n It will expire in ${expireDate}`,
     });
   } catch (err) {
     console.log('error while sending mail', err);
