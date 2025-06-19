813a47df3f75567e91aab94456efe4bf4ffee1fc
rdsemon <codewithemon@gmail.com>
2025-06-19 21:07:28 +0600

    fixed the router method and add some new route

--- backend/routers/userRoute.js
+++ backend/routers/userRoute.js
@@ -6,14 +6,28 @@ const authController = require('../controllers/authController');
 
 const userController = require('../controllers/userController');
 
-const { signup, login, forgetPassword } = authController;
+const {
+  signup,
+  login,
+  forgetPassword,
+  resetPassword,
+  updatePassword,
+  protect,
+} = authController;
 
-const { createUser } = userController;
+const { createUser, getAllUsers, deActiveMe, reActivateMe, updateMe } =
+  userController;
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/forgetpassword', forgetPassword);
+router.patch('/resetpassword/:token', resetPassword);
+router.patch('/ReActiveMe', reActivateMe);
 
-router.route('/').post(createUser);
+router.patch('/updatepassword', protect, updatePassword);
+router.patch('/deActiveMe', protect, deActiveMe);
+router.patch('/updateMe', protect, updateMe);
+
+router.route('/').post(createUser).get(getAllUsers);
 
 module.exports = router;
