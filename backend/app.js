3b7b3c2714039bb34dd66761ca6f03b78020283c
rdsemon <codewithemon@gmail.com>
2025-06-19 21:12:03 +0600

    add ratelimiter, mongosanitizer, helmet for security

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const ratelimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongosnitize = require('express-mongo-sanitize');
 const morgan = require('morgan');
 const pizzaRoute = require('./routers/pizzaRoute');
 const globalErrorHandaler = require('./controllers/errorController');
@@ -10,14 +13,26 @@ dotenv.config({ path: './config.env' });
 
 const app = express();
 
+app.use(helmet());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 console.log(process.env.NODE_ENV);
 
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 
+app.use(mongosnitize());
+
+const limiter = ratelimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: 'draft-8',
+  legacyHeaders: false,
+  message: 'Too many request from this ip. Please try again 15 minutes letter',
+});
+
+app.use('/api', limiter);
 app.use('/api/v1/pizza', pizzaRoute);
 app.use('/api/v1/user', userRouter);
 
