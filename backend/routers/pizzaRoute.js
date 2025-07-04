743672861587a9984f170930221b35bc941ce0e3
rdsemon <codewithemon@gmail.com>
2025-07-04 21:42:28 +0600

    implement nested route with reviewRoute

--- backend/routers/pizzaRoute.js
+++ backend/routers/pizzaRoute.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
-const pizzaController = require('../controllers/pizzaController');
-
 const router = express.Router();
+
+const pizzaController = require('../controllers/pizzaController');
+// const authController = require('../controllers/authController');
+const reviewRoute = require('./reviewRoute');
 
 const { getAllpizza, getOnePizza, createPizza, updatePizza, deletePizza } =
   pizzaController;
 
+// const { protect } = authController;
+router.use('/:pizzaId/review', reviewRoute);
 router.route('/').get(getAllpizza).post(createPizza);
 
 router.route('/:id').get(getOnePizza).patch(updatePizza).delete(deletePizza);
