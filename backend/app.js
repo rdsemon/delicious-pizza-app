122341ec04cd69a2873f5bbde2cd9f60180297e1
rdsemon <codewithemon@gmail.com>
2025-07-04 21:39:55 +0600

    add new riview router and security function

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,17 @@ const dotenv = require('dotenv');
 const express = require('express');
 const ratelimit = require('express-rate-limit');
 const helmet = require('helmet');
-const mongosnitize = require('express-mongo-sanitize');
+
 const morgan = require('morgan');
+
+// express router module
 const pizzaRoute = require('./routers/pizzaRoute');
+const userRouter = require('./routers/userRoute');
+const reviewRouter = require('./routers/reviewRoute');
+// error handeling module
+const sanitize = require('./utils/sanitize');
 const globalErrorHandaler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
-const userRouter = require('./routers/userRoute');
 
 dotenv.config({ path: './config.env' });
 
@@ -22,7 +27,13 @@ console.log(process.env.NODE_ENV);
 
 app.use(express.json({ limit: '10kb' }));
 
-app.use(mongosnitize());
+app.use((req, res, next) => {
+  if (req.body) req.body = sanitize(req.body);
+  if (req.params) req.params = sanitize(req.params);
+  if (req.query) req.query = sanitize(req.query);
+
+  next();
+});
 
 const limiter = ratelimit({
   windowMs: 15 * 60 * 1000,
@@ -35,6 +46,7 @@ const limiter = ratelimit({
 app.use('/api', limiter);
 app.use('/api/v1/pizza', pizzaRoute);
 app.use('/api/v1/user', userRouter);
+app.use('/api/v1/review', reviewRouter);
 
 app.all(/(.*)/, (req, res, next) => {
   next(new AppError(`can't find url ${req.originalUrl} route`, 404));
